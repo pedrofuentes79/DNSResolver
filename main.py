0d7f910635c970ec05b46b64787d1d321eea908d
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-16 13:11:41 -0300

    reorganized content

--- main.py
+++ main.py
@@ -4,45 +4,10 @@ import dataclasses
 from io import BytesIO
 from dataclasses import dataclass
 import dns.resolver
+from parsers import *
+from classes import *
+from constants import *
 
-#constants
-TYPE_A = 1
-TYPE_NS = 2
-TYPE_CNAME = 5
-TYPE_AAAA = 28 #ipv6 adress
-TYPE_SOA = 6
-
-@dataclass
-class Header:
-    # Here we are ignoring most flags :)
-    id: int
-    flags: int
-    num_questions: int = 0
-    num_answers: int = 0
-    num_authorities: int = 0
-    num_additional: int = 0
-
-@dataclass
-class Question:
-    name: bytes # youtube.com
-    type_: int  # A
-    class_: int # (always the same)
-
-@dataclass
-class Record:
-    name: bytes   # domain name
-    type_: int    # A, AAAA, MX... (encoded as int)
-    class_: int   # Always 1
-    ttl: int      # How long to cache the query for. We'll ignore this
-    data: bytes   # The record's content, like the IP address
-
-@dataclass
-class Packet:
-    header: Header
-    questions: list[Question]
-    answers: list[Record]
-    authorities: list[Record]
-    additional: list[Record]
 
 def header_to_bytes(header):
     # This function converts classes to byte strings
@@ -95,50 +60,6 @@ def send_query(ip_address="8.8.8.8", domain_name="dns.google.com", record_type=1
 
     data, _ = sock.recvfrom(1024)
     return parse_packet(data)
-
-def parse_header(reader):
-    # !HHHHHH means that we're expecting 6 unsigned shorts (2 bytes each)
-    # We read 12 bytes because we have 6*2 bytes in total
-    items = struct.unpack('!HHHHHH', reader.read(12))
-    return Header(*items) 
-
-def parse_question(reader):
-    name = decode_name(reader)
-    data = reader.read(4)
-    type_, class_ = struct.unpack('!HH', data)
-    return Question(name, type_, class_)
-
-def parse_record(reader):
-    name = decode_name(reader)
-    data = reader.read(10)
-    # HHIH means 2byte int, 2byte int, 4byte int, 2byte int
-    type_, class_, ttl, data_length = struct.unpack('!HHIH', data)
-    if type_ == TYPE_NS:
-        data = decode_name(reader)
-    elif type_ == TYPE_A:
-        data = ip_to_string(reader.read(data_length))
-    elif type_ == TYPE_CNAME:
-        # CNAME is an alias, so we decode it as a name
-        # then, we can resolve the name to an IP address
-        cname_domain = decode_name(reader)
-        data = resolve(cname_domain, TYPE_CNAME)
-    else:
-        data = reader.read(data_length)
-    return Record(name, type_, class_, ttl, data)
-
-
-def parse_packet(data):
-    reader = BytesIO(data)
-    # puts all pieces of the packet together
-    header = parse_header(reader)
-    questions = [parse_question(reader) for _ in range(header.num_questions)]
-    answers = [parse_record(reader) for _ in range(header.num_answers)]
-    authorities = [parse_record(reader) for _ in range(header.num_authorities)]
-    additional = [parse_record(reader) for _ in range(header.num_additional)]
-
-    return Packet(header, questions, answers, authorities, additional)
-
-
 
 def decode_name_simple(reader):
     # This function fails if the name is compressed
